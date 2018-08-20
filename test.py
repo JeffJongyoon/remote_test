c98ab1e2912b7209001cc1df4f1fdbf1a9bd122d
Jongyoon Sohn <jysohn0104@gmail.com>
2018-08-20 19:47:46 -0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,31 @@
+import sys
+print(sys.executable)
+print(sys.version)
+
+class Employee:
+    """A sample employee class"""
+
+    def __init__(self, first, last):
+        self.first=first
+        self.last=last
+
+    @property
+    def email(self):
+        return '{}.{}@email.com'.format(self.first,self.last)
+
+    @property
+    def fullname(self):
+        return '{} {}'.format(self.first,self.last)
+
+
+for num in [1,2,3,4,5,6]:
+    print(num)
+
+emp_1=Employee('Jongyoon','Sohn')
+
+print(emp_1.first)
+print(emp_1.last)
+print(emp_1.fullname)
+print(emp_1.email)
+
+
